c7a3dee81ecf14da04872a93d03f7b27e58f4d1f
Aditya Sharat <adityasharat@gmail.com>
2017-10-11 17:43:33 +0530

    add index.js

---
+++ client/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import registerServiceWorker from './registerServiceWorker';
+import thunk from 'redux-thunk'
+
+import './index.css';
+import reducer from '../src/reducers/index'
+
+const logger = store => next => action => {
+  //console.group(action.type);
+  //console.info('dispatching', action);
+  let result = next(action);
+  //console.log('next state', store.getState());
+  //console.groupEnd(action.type);
+  return result;
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const reduxDevTools = composeEnhancers(applyMiddleware(logger, thunk));
+
+const store = createStore(reducer, reduxDevTools)
+
+const template = (
+<BrowserRouter>
+  <Provider store={store}>
+    <h1>Hello World</h1>
+  </Provider>
+</BrowserRouter>
+);
+
+ReactDOM.render(template, document.getElementById('root'));
+registerServiceWorker();
