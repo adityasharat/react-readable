4ebaa28d5a494a5599e0b686f410b2c9eeb54a2c
Aditya Sharat <adityasharat@gmail.com>
2017-10-12 14:36:58 +0530

    Add reducers for categories

---
+++ client/src/reducers/Categories.js
@@ -0,0 +1,15 @@
+import * as Types from '../actions/ActionTypes'
+
+function categories(state = [], action) {
+
+  switch(action.type) {
+
+    case Types.FETCH_CATEGORY:
+      return action.res.categories;
+
+    default:
+      return state;
+  }
+}
+
+export default categories
