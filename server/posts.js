a3c79e8b745aaa8f8fccdb382e97349ec6cb0b47
Aditya Sharat <adityasharat@gmail.com>
2017-11-17 14:17:02 +0530

    add semi colons

--- server/posts.js
+++ server/posts.js
@@ -1,6 +1,6 @@
-const clone = require('clone')
+const clone = require('clone');
 
-let db = {}
+let db = {};
 
 const defaultData = {
   "8xf0y6ziyjabvozdd253nd": {
@@ -11,7 +11,7 @@ const defaultData = {
     author: 'thingtwo',
     category: 'react',
     voteScore: 6,
-    deleted: false 
+    deleted: false
   },
   "6ni6ok3ym7mf1p33lnez": {
     id: '6ni6ok3ym7mf1p33lnez',
@@ -23,49 +23,45 @@ const defaultData = {
     voteScore: -5,
     deleted: false
   }
-}
+};
 
 function getData (token) {
-  let data = db[token]
+  let data = db[token];
   if (data == null) {
-    data = db[token] = clone(defaultData)
+    data = db[token] = clone(defaultData);
   }
-  return data
+  return data;
 }
 
 function getByCategory (token, category) {
   return new Promise((res) => {
-    let posts = getData(token)
-    let keys = Object.keys(posts)
-    let filtered_keys = keys.filter(key => posts[key].category === category && !posts[key].deleted)
-    res(filtered_keys.map(key => posts[key]))
-  })
+    let posts = getData(token);
+    let keys = Object.keys(posts);
+    let filtered_keys = keys.filter(key => posts[key].category === category && !posts[key].deleted);
+    res(filtered_keys.map(key => posts[key]));
+  });
 }
 
 function get (token, id) {
   return new Promise((res) => {
-    const posts = getData(token)
-    res(
-      posts[id].deleted 
-        ? {}
-        : posts[id]
-    )
-  })
+    const posts = getData(token);
+    res(posts[id].deleted ? {} : posts[id]);
+  });
 }
 
 function getAll (token) {
   return new Promise((res) => {
-    const posts = getData(token)
-    let keys = Object.keys(posts)
-    let filtered_keys = keys.filter(key => !posts.deleted)
-    res(filtered_keys.map(key => posts[key]))
-  })
+    const posts = getData(token);
+    let keys = Object.keys(posts);
+    let filtered_keys = keys.filter(key => !posts.deleted);
+    res(filtered_keys.map(key => posts[key]));
+  });
 }
 
 function add (token, post) {
   return new Promise((res) => {
-    let posts = getData(token)
-    
+    let posts = getData(token);
+
     posts[post.id] = {
       id: post.id,
       timestamp: post.timestamp,
@@ -75,46 +71,46 @@ function add (token, post) {
       category: post.category,
       voteScore: 1,
       deleted: false
-    }
-     
-    res(posts[post.id])
-  })
+    };
+
+    res(posts[post.id]);
+  });
 }
 
 function vote (token, id, option) {
   return new Promise((res) => {
-    let posts = getData(token)
-    post = posts[id]
+    let posts = getData(token);
+    post = posts[id];
     switch(option) {
         case "upVote":
-            post.voteScore = post.voteScore + 1
-            break
+            post.voteScore = post.voteScore + 1;
+            break;
         case "downVote":
-            post.voteScore = post.voteScore - 1
-            break
+            post.voteScore = post.voteScore - 1;
+            break;
         default:
-            console.log(`posts.vote received incorrect parameter: ${option}`)
+            console.log(`posts.vote received incorrect parameter: ${option}`);
     }
-    res(post)
-  })
+    res(post);
+  });
 }
 
 function disable (token, id) {
     return new Promise((res) => {
-      let posts = getData(token)
-      posts[id].deleted = true
-      res(posts[id])
+      let posts = getData(token);
+      posts[id].deleted = true;
+      res(posts[id]);
     })
 }
 
 function edit (token, id, post) {
     return new Promise((res) => {
-        let posts = getData(token)
+        let posts = getData(token);
         for (prop in post) {
-            posts[id][prop] = post[prop]
+            posts[id][prop] = post[prop];
         }
-        res(posts[id])
-    })
+        res(posts[id]);
+    });
 }
 
 module.exports = {
@@ -126,4 +122,4 @@ module.exports = {
   disable,
   edit,
   getAll
-}
+};
