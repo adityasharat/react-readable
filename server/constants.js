1b83f9daed2a913176e3f61f92a600dfbf6948b6
Aditya Sharat <adityasharat@gmail.com>
2017-11-17 14:31:39 +0530

    add constants file

---
+++ server/constants.js
@@ -0,0 +1,10 @@
+const MESSAGES = {
+  ERRORS: {
+    DEFAULT: 'There was an error.',
+    AUTHORIZATION: 'Please provide an Authorization header to identify yourself (can be whatever you want)'
+  }
+};
+
+module.exports = {
+  MESSAGES
+};
